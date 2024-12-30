ccdc1f99a9863d3524f45c16cee29877b2cf41c7
marianabragagnolo <marianasouzablumenau@gmail.com>
2024-12-30 13:35:30 -0300

    4 operações matemáticas prontas

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ print('Hello, World!')
 def soma(a: int, b: int):
     return a + b
 
+def sub(a: int, b: int):
+    return a - b
+
+def div(a: int, b: int):
+    return (a / b)
+
+def mult(a: int, b: int):
+    return a * b
 
 
-valor = soma(5, 7)
+
+valor = soma(5, 5)
 print(f'valor da soma é {valor}')
